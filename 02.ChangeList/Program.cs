61b699f7b7161724c57affc70ac99dab87e9c7b1
AnitoKarakasheva <134801293+AnitoKarakasheva@users.noreply.github.com>
2023-10-20 11:06:33 +0300

    Add project files.

---
+++ 02.ChangeList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] passengers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int maxCapacity = int.Parse(Console.ReadLine());
+
+            List<int> wagons = new List<int>(passengers);
+
+            string commands;
+            while ((commands = Console.ReadLine()) != "end")
+            {
+                string[] arguments = commands.Split();
+                if (arguments[0] == "Add")
+                {
+                    wagons.Add(int.Parse(arguments[1]));
+                }
+                else
+                {
+                    int newPassengers = int.Parse(arguments[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + newPassengers <= maxCapacity)
+                        {
+                            wagons[i] += newPassengers;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
+    
